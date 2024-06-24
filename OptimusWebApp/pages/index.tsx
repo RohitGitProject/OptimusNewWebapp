111a0773b0a381d4b5d366b3cd969f69806b9f92
Rohit <rohit.patil@paramlogic.com>
2024-06-24 11:31:17 +0530

    name

--- OptimusWebApp/pages/index.tsx
+++ OptimusWebApp/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to My Fullstack Project</h1>
+      <h1>Welcome to My First Project</h1>
     </div>
   );
 };
